bfc18adfd3964659e4784fd7bdddecf86d4310a6
Shalom <shalom.pelles@gmail.com>
2020-10-07 00:21:59 +0300

    [refactor] length property calculates the result

--- index.ts
+++ index.ts
@@ -15,5 +15,8 @@ export class Bavli {
     return this._pageIndex;
   }
 
-  length = 2711;
+  // eslint-disable-next-line max-len
+  private lengths = [63, 156, 104, 120, 21, 34, 87, 55, 39, 30, 31, 28, 26, 121, 111, 90, 65, 48, 89, 81, 118, 118, 175, 112, 23, 48, 75, 13, 119, 109, 141, 60, 33, 33, 27, 20, 3, 4, 9, 72];
+
+  length = this.lengths.reduce((prev, curr) => prev + curr, 0);
 }
